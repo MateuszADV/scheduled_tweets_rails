e9b195284f138d2864efdb8d7df922e786fcfe5d
MateuszADV <mateusz.adv@gmail.com>
2021-03-02 20:21:03 +0100

    Send token to  reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,5 @@
 class PasswordResetsController < ApplicationController
     def new
-
     end
 
     def create
@@ -9,9 +8,31 @@ class PasswordResetsController < ApplicationController
         if @user.present?
             # send emali
             PasswordMailer.with(user: @user).reset.deliver_now
-            redirect_to root_path, notice: "Na podany mail został wysłany link do resetu hasla."
-        else
-            redirect_to root_path, notice: "Podany email istnieje w bazie, został wysłany link do resetu hasla."
+            redirect_to root_path, notice: "Na podany mail zostal wysłany link do resetu hasla."
+        # else
+        #     redirect_to root_path, notice: "Podany email istnieje w bazie, został wysłany link do resetu hasla."
         end
+    end
+
+    def edit 
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+        # binding.irb wyswie
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: " Twoj token wygasl, ponow probę ponownie"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: " twoje haslo zostalo zmienione. Zaloguj sie"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
